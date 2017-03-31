77541d051b11e6c4d622966c33bd741a46b63983
Mario Junior <mario_souza.junior@hotmail.com>
2017-03-30 22:24:23 -0300

    Iniciada Novas View
    Foi iniciada a nova view da Home do Evento

---
+++ ISports/Controllers/EventHomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISports.Controllers
+{
+    public class EventHomeController : Controller
+    {
+        // GET: EventHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
