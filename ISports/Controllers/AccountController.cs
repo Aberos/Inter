59e9ab753f8acffbe01f39dd7c5e4dfff7fc3067
Mario Junior <mario_souza.junior@hotmail.com>
2017-04-15 12:24:31 -0300

    Organização
    Organizado os controllers

---
+++ ISports/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISports.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        public ActionResult Recover()
+        {
+            return View();
+        }
+    }
+}
---
+++ ISports/Controllers/EventController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISports.Controllers
+{
+    public class EventController : Controller
+    {
+        // GET: Evento
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult Manager()
+        {
+            return View();
+        }
+
+        public ActionResult Search()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult FeedEvents()
+        {
+            return View();
+        }
+
+        public ActionResult UserEvents()
+        {
+            return View();
+        }
+
+    }
+}
