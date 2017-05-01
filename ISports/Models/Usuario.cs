7ce3a53667bef4d22f26ceeecd5ac143fc46e2f8
Vau <verners_sccp@hotmail.com>
2017-05-01 14:12:02 -0300

    Validações
    Validações razor na model usuário

--- ISports/Models/Usuario.cs
+++ ISports/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -12,19 +13,29 @@ namespace ISports.Models
         public int Id_usuario { get; set; }
 
         public int Estatus { get; set; }
-
+        
+        [Required]
+        [DisplayName("E-mail")]
         public string Email { get; set; }
 
         [Required]
-
+        [DisplayName("First Name")]
         public string Nome { get; set; }
 
+        [Required]
+        [DisplayName("Last Name")]
         public string Sobrenome { get; set; }
 
+        [Required]
+        [DisplayName("Birth Date")]
         public DateTime DataNasc { get; set; }
 
+        [Required]
+        [DisplayName("Cellphone")]
         public string Cel { get; set; }
 
+        [Required]
+        [DisplayName("Password")]
         public string Senha { get; set; }
 
         public byte[] Foto_Perfil { get; set; }
