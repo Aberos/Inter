10905b32e0d97002bff4a04ad2a8e171a3068a1c
Mario Junior <mario_souza.junior@hotmail.com>
2017-05-04 10:19:26 -0300

    Inserts Teste
    Inserts feitos no banco para testar a criação de evento

---
+++ Inserts Teste.sql
@@ -0,0 +1,18 @@
+insert into ufs values ('SP','São Paulo') 
+insert into ufs values ('AC','Acre') 
+insert into ufs values ('RJ','Rio de Janeiro') 
+insert into ufs values ('MG','Minas Gerais') 
+
+
+
+insert into cidades values ('São Paulo','SP') 
+insert into cidades values ('São José do Rio Preto','SP') 
+insert into cidades values ('Onda Verde','SP') 
+insert into cidades values ('Rio Branco','AC') 
+
+
+
+insert into esportes values ('Football') 
+insert into esportes values ('Tennis') 
+insert into esportes values ('Basktball') 
+insert into esportes values ('Volleyball') 
